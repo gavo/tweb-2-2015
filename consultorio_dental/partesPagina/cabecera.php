a6f52d94307fe5bdef4dd7a9812defb049056dbd
Gustavo Vargas Miranda <gustavo@proemex.com>
2015-11-12 14:39:32 -0400

    Consultorio Dental: Agregando Cabecera, pie de pagina e inicio del estilo.css

---
+++ consultorio_dental/partesPagina/cabecera.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+	<meta http-equiv = 'Content-Type' 	content = 'text/html; charset=utf-8'>
+  	<meta name="Author" content="Gimena Burneett">
+  	<meta name="Keywords" content="javascript, html">
+  	<meta name="Description" content="Proyecto Final Tecnología Web">
+  	<link href="estilo.css" rel="stylesheet" type="text/css">
+    <title>Consultorio Dental</title>
+</head>
+    
