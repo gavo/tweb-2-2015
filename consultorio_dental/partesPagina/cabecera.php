a16f2bd13fbf94a2d57048c27dfccf253d2a4a71
Gustavo Vargas Miranda <gvmgust@hotmail.com>
2015-11-12 18:22:47 -0400

    Consultorio Dental: Definiendo Cabecera completa con barra de navegación y ademas el pie de pagina

--- consultorio_dental/partesPagina/cabecera.php
+++ consultorio_dental/partesPagina/cabecera.php
@@ -4,7 +4,21 @@
   	<meta name="Author" content="Gimena Burneett">
   	<meta name="Keywords" content="javascript, html">
   	<meta name="Description" content="Proyecto Final Tecnología Web">
+    <link rel = 'shortcut icon' href = 'img/icon.gif'> 
   	<link href="estilo.css" rel="stylesheet" type="text/css">
     <title>Consultorio Dental</title>
 </head>
+<body>
+    <header>
+        <img src="img/logo.png">
+    </header>
+    <nav>  
+        <a href="<?php echo $categorias;?>">Inicio</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="<?php echo $categorias;?>">Registrar Usuario</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="<?php echo $buscar;?>">Reservar Consulta</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="<?php echo $categorias;?>">Ver Citas</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="<?php echo $buscar;?>">Atender Citas</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="<?php echo $host;?>">Iniciar Sesion</a>&nbsp;&nbsp;&nbsp;&nbsp; 
+    </nav>
+    <div id='cuerpo_pagina'>
     
